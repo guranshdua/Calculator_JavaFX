00152d94d0a77133cd2a5fe8dce00ebea99dbe2b
Guransh Dua <guransh.dua1@gmail.com>
2020-03-18 20:34:04 +0530

    Basic Calculator Created

---
+++ src/application/MainController.java
@@ -0,0 +1,54 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+public class MainController {
+	@FXML
+	private Label result;
+	private long number1=0;
+	private String operator="";
+	private boolean start=true;
+	Model model=new Model();
+	@FXML
+	public void processNumbers(ActionEvent event)
+	{
+		if(start)
+		{
+			result.setText("");
+			start=false;
+		}
+		String value=((Button)event.getSource()).getText();
+		result.setText(result.getText()+value);
+	}
+	
+	@FXML
+	public void processOperators(ActionEvent event)
+	{
+		String value=((Button)event.getSource()).getText();
+		if(!value.equals("="))
+		{
+			if(!operator.isEmpty())
+			{
+				return;
+			}
+			operator=value;
+			number1=Long.parseLong(result.getText());
+			result.setText("");
+		}
+		else
+		{
+			if(operator.isEmpty())
+			{
+				return;
+			}
+			long number2=Long.parseLong(result.getText());
+			float var=model.calculate(number1, number2, operator);
+			result.setText(String.valueOf(var));
+			operator="";
+			start=true;
+		}
+	}
+}
